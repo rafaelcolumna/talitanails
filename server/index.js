6c20ac7e217b3d2f5f967397fa15a714b3cd5744
rafaelcolumna <70344264+rafaelcolumna@users.noreply.github.com>
2021-08-15 20:29:26 -0400

    started my backend file structure. added models and controllers

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded);
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://talitanails:Guardia75@talitanails.c0fz7.mongodb.net/talitanails?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect (CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
