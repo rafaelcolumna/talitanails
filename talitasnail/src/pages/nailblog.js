f3630884dfab9c52bcc7e93ab09537ff656e200a
rafaelcolumna <70344264+rafaelcolumna@users.noreply.github.com>
2021-08-20 12:50:02 -0400

    added new routes to update delete and add. also added material ui icons for new look improvement.

--- talitasnail/src/pages/nailblog.js
+++ talitasnail/src/pages/nailblog.js
@@ -30,13 +30,13 @@ function Nailblog() {
             <div>
                 <Grow in>
                     <Container>
-                        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form />
-                        </Grid>
+                        <Grid container justifyContent="space-evenly" alignItems="stretch" spacing={3}>
+                            <Grid item xs={12} sm={7}>
+                                 <Posts />
+                            </Grid>
+                            <Grid item xs={12} sm={4}>
+                                 <Form />
+                            </Grid>
                         </Grid>  
                     </Container>
                 </Grow>
